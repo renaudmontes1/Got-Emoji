39d18a34c3ee661e797fcf283a1271818b516adf
Renaud Montes <rmontesl@hotmail.com>
2025-10-29 21:11:31 -0700

    Initial Commit

---
+++ Got Emoji Watch App/Got_EmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Got_EmojiApp.swift
+//  Got Emoji Watch App
+//
+//  Created by Admin on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Got_Emoji_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Got Emoji/Got_EmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Got_EmojiApp.swift
+//  Got Emoji
+//
+//  Created by Admin on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Got_EmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
